3f30b206960976e9888f19da3a9de163f0738895
jufeta87 <jufe357@hotmail.com>
2020-05-25 15:08:50 -0500

    se realiza la botonera para encendido y apagado de los led en las dos api de rasberry de juan y sebastian

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { HomeComponent } from './home/home.component';
-import { CamaraComponent } from '../camara/camara.component';
-
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { HomeComponent } from "./home/home.component";
+import { CamaraComponent } from "../camara/camara.component";
+import { LuminariasComponent } from "../luminarias/luminarias.component";
 
 const routes: Routes = [
-  { path:"", component: HomeComponent},
-  { path: "camara", component: CamaraComponent }
+  { path: "", component: HomeComponent },
+  { path: "camara", component: CamaraComponent },
+  { path: "luminaria", component: LuminariasComponent }
 ];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
-  exports: [NativeScriptRouterModule]
+  exports: [NativeScriptRouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
